c44729a9a22a23d2050e5a19be0a75b5d27e77d5
Jean-Pierre <113295208+JPMulder2003@users.noreply.github.com>
2022-12-16 20:17:43 +0200

    Update Countdown
    Working on resolving bugs in my final project

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Dec 16, 2022 20:00:00").getTime();
+var countDownDate = new Date("Dec 18, 2022 20:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
